4da5ef716f3abd6c8ba650a0ba0c0c90e44cd945
ahmed essam <ahmed.essam202062@gmail.com>
2023-06-08 16:34:18 +0300

    last one

--- adventerous_game.py
+++ adventerous_game.py
@@ -22,12 +22,14 @@ def start_game():
     score = 0
     # Define the starting point of the story
     print_Pause(
-        f"Welcome,{player_name}!,Nice {Specific_Color} outfit You find yourself in a dark forest"
+        f"Welcome,{player_name}!"
     )
-
     print_Pause(
-        f"searching for a lost treasure. As you make your way through the trees, you come across a fork in the path.Your current score is {score}"
+        f"Nice {Specific_Color} outfit You find yourself in a dark forest")
+    print_Pause(
+        f"searching for alost treasure. As you make your way through the trees"
     )
+    print_Pause(f"you come across afork in the path.Your score is {score}")
 
     # Define the first choice
 
@@ -42,12 +44,15 @@ def start_game():
     if choice1 == "left":
         score += 10
         print_Pause(
-            f"\nYou choose to take the left path,you come across a clearing with a small pond. There isa small boat tied up on the shore.Your score is {score}"
+            f"\nYou choose to take the left path, "
         )
+        print_Pause("you come across a clearing with a small pond.")
+        print_Pause(
+            f"There isa small boat tied up on the shore.Your score is {score}")
 
         while True:
             choice2 = input(
-                "Do you want to get in the boat and row across the pond? (yes / no)"
+                "Do you want to get in boat and row across the pond?(yes/no)"
             )
             if choice2.lower() in ["yes", "no"]:
                 break
@@ -58,32 +63,38 @@ def start_game():
             score += 10
             print_Pause(f"\nYou get into the boat and row across the pond.")
             print_Pause(
-                f"As you approach the other side, you see a glint of gold in the distance."
+                f"As you approach the other side"
             )
+            print_Pause("you see a glint of gold in the distance")
             print_Pause(
-                f"You row closer and find a treasure chest filled with gold and jewels!"
+                f"You row closer"
             )
+            print_Pause("and find atreasure chest filled with gold and jewels")
             print_Pause(
-                f"Congratulations, adventurer! You have earned 10 points. Your score is {score}"
+                f"You have earned 10 points.Your score is {score}"
             )
 
             End_Game(player_name, score)
         else:
             score -= 10
             print_Pause(
-                f"\nYou decide not to risk getting in the boat, hoping to find the treasure another way.Your score is {score}"
+                f"\nYou decide not to risk getting in the boat"
             )
+            print_Pause(f"hoping to find the treasure another way.")
+            print_Pause(f"Your score is {score}")
             End_Game(player_name, score)
     # Consequences based on the right choice
     else:
         score += 10
         print_Pause(f"\nYou choose to take the right path")
         print_Pause(
-            f"you come across a dark and spooky cave.A faint light can be seen in the distance.Your score is {score}"
+            f"you come across a dark and spooky cave"
         )
+        print_Pause(f".A faint light can be seen in the distance.")
+        print_Pause(f"Your score is {score}")
         while True:
             choice2 = input(
-                "Do you want to enter the cave and investigate the light?(yes/no)"
+                "Do you want to enter and investigate the light?(yes/no)"
             )
             if choice2 in ["yes", "no"]:
                 break
@@ -94,21 +105,25 @@ def start_game():
         if choice2 == "yes":
             score += 10
             print_Pause(
-                f"\nYou enter the cave and make your way through the twisting passages"
+                f"You entered and make your way through the twisting passages"
             )
             print_Pause(
-                f"Eventually,you come across aroom with asmall fire burning in the center"
+                f"Eventually,you come across aroom"
             )
+            print_Pause("with asmall fire burning in the center")
             print_Pause(
-                f"Next to the fire is a treasure chest filled with gold and jewels!"
+                f"Next to the fire is a treasure chest filled with gold!"
             )
             print_Pause(f"You have earned 10 points.Your score is {score}.")
+            End_Game(player_name, score)
 
         else:
             score -= 10
             print_Pause(
-                f"\nYou decide not to risk entering the cave,hoping to find the treasure another way.Your score is {score}"
+                f"You decide not to risk entering the cave"
             )
+            print_Pause(f"hoping to find the treasure another way")
+            print_Pause(f"Your score is {score}")
             End_Game(player_name, score)
             # End Game
 
@@ -116,12 +131,14 @@ def start_game():
 def End_Game(player_name, score):
     if score == 20:
         print_Pause(
-            f"\nCongratulations,{player_name}!You have completed the game with score of 40."
+            f"Congratulations,{player_name}"
         )
+        print_Pause("You have completed the game with score of 40")
         print_Pause(f"/nThanks for playing!")
         Repeating_Game()
     else:
-        print_Pause(f"/nGood luck for another timeYour current score is {score}")
+        print_Pause(
+            f"/nGood luck for another timeYour current score is {score}")
         Repeating_Game()
 
 
